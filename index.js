47fda448c94e1fe5ad3d02d097dfb79900c57e04
Sumaine210 <jordanderby18@gmail.com>
2021-12-29 11:10:14 -0600

    Add a function to get a random choice of each component in the message

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const greetings = ['Hey','Hello','Hi','Whats up','Yo'];
 const identifiers = ['friend','you','buddy','pal'];
 const phrases = ['you look great!','How have you been?',"isn't it a nice day?","it's good to see you.",];
 const sentenceArrays = [greetings,identifiers,phrases,];
+
+const getRandomMessage = nestedArray => {
+    nestedArray.forEach(array => {
+        let component = array[Math.floor(Math.random() * array.length)];
+        console.log(component)
+    });
+};
+getRandomMessage(sentenceArrays)
