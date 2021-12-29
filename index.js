ee1c2c5724c7c1526741b5cd563d534223b5676d
Sumaine210 <jordanderby18@gmail.com>
2021-12-29 11:35:40 -0600

    Update function to join each component to create the full message in one string

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
 const greetings = ['Hey','Hello','Hi','Whats up','Yo'];
 const identifiers = ['friend','you','buddy','pal'];
-const phrases = ['you look great!','How have you been?',"isn't it a nice day?","it's good to see you.",];
+const phrases = ['you look great!','how have you been?',"isn't it a nice day?","it's good to see you.",];
 const sentenceArrays = [greetings,identifiers,phrases,];
 
 const getRandomMessage = nestedArray => {
+    let completeSentence = []
     nestedArray.forEach(array => {
-        let component = array[Math.floor(Math.random() * array.length)];
-        console.log(component)
+         completeSentence.push(array[Math.floor(Math.random() * array.length)]);
     });
+    return `${completeSentence[0]} ${completeSentence[1]}, ${completeSentence[2]}`
 };
-getRandomMessage(sentenceArrays)
+getRandomMessage(sentenceArrays);
