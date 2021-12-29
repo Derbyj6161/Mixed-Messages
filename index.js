9dee146273c1908d42373b247f328d78fa3142d9
Sumaine210 <jordanderby18@gmail.com>
2021-12-29 10:24:26 -0600

    Create arrays for each components of the message

---
+++ index.js
@@ -0,0 +1,5 @@
+
+const greetings = ['Hey','Hello','Hi','Whats up','Yo'];
+const identifiers = ['friend','you','buddy','pal'];
+const phrases = ['you look great!','How have you been?',"isn't it a nice day?","it's good to see you.",];
+const sentenceArrays = [greetings,identifiers,phrases,];
