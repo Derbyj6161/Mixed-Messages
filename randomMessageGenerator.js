82e4fb348bc8d99d7d5fa21d46681fe44326a089
Sumaine210 <jordanderby18@gmail.com>
2022-02-23 00:17:07 -0600

    add html file and link to js file

--- randomMessageGenerator.js
+++ randomMessageGenerator.js
@@ -3,12 +3,15 @@ const greetings = ['Hey','Hello','Hi','Whats up',];
 const identifiers = ['friend','you','buddy','pal'];
 const phrases = ['you look great!','how have you been?',"isn't it a nice day?","it's good to see you.",];
 const sentenceArrays = [greetings,identifiers,phrases,];
+let loadText = document.querySelector('.greeting');
 
 const getRandomMessage = nestedArray => {
     let completeSentence = []
     nestedArray.forEach(array => {
          completeSentence.push(array[Math.floor(Math.random() * array.length)]);
     });
-    console.log(`${completeSentence[0]} ${completeSentence[1]}, ${completeSentence[2]}`)
+    return `${completeSentence[0]} ${completeSentence[1]}, ${completeSentence[2]}`;
 };
-getRandomMessage(sentenceArrays);
+
+loadText.innerText = getRandomMessage(sentenceArrays)
+
