46224794a103256e37ba18a18c8fd576eec400a5
Sumaine210 <jordanderby18@gmail.com>
2022-02-23 02:16:07 -0600

    add refresh button and function

--- randomMessageGenerator.js
+++ randomMessageGenerator.js
@@ -4,6 +4,7 @@ const identifiers = ['friend','you','buddy','pal'];
 const phrases = ['you look great!','how have you been?',"isn't it a nice day?","it's good to see you.",];
 const sentenceArrays = [greetings,identifiers,phrases,];
 let loadText = document.querySelector('.greeting');
+let refreshButton = document.querySelector('.refresh')
 
 const getRandomMessage = nestedArray => {
     let completeSentence = []
@@ -13,5 +14,17 @@ const getRandomMessage = nestedArray => {
     return `${completeSentence[0]} ${completeSentence[1]}, ${completeSentence[2]}`;
 };
 
+//initial greeting
 loadText.innerText = getRandomMessage(sentenceArrays)
+
+//refresh greeting button
+refreshButton.addEventListener('click', () => {
+    let oldText = loadText.innerText
+    let newText = getRandomMessage(sentenceArrays);
+    if(newText === oldText){
+        newText = getRandomMessage(sentenceArrays);
+        loadText.innerText = newText;
+    } else {loadText.innerText = newText}
+})
+
 
